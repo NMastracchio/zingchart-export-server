4110d33fdaa2ee7a53126a8bd180171a93a83c08
Nicholas Mastracchio <nicholas.mastracchio@gmail.com>
2015-07-31 13:42:59 -0700

    Fixed formatting

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var app = express();
 var zingchartExport = require('./zingchart-export.js');
 
 var server = app.listen(3000, function () {
-	var port = server.address().port;
-	console.log('Example app listening at http://localhost:%s', port);
+  var port = server.address().port;
+  console.log('Example app listening at http://localhost:%s', port);
 });
 
 app.post('/', function (req, res) {
-		zingchartExport(req, res, function(err, data){
-			if (err) throw err;
-			console.log(data);
-		});
+    zingchartExport(req, res, function(err, data){
+      if (err) throw err;
+      console.log(data);
+    });
 });
